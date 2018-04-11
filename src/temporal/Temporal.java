8f2e0b907b7d5bd9b257bf0fefd24ba8146a2bd5
Admin <Admin@192.168.1.7>
2018-04-11 15:16:15 -0600

    Este es un comentario sobre lo nuevo

---
+++ src/temporal/Temporal.java
@@ -0,0 +1,17 @@
+
+package temporal;
+
+/**
+ *
+ * @author Admin
+ */
+public class Temporal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
